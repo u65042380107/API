8410e05237e148bba72dd1e45bbaf99f47ffaaaf
Benz Natthaphong <useforstudybenz@gmail.com>
2024-08-01 15:59:58 +0700

    convert to netlify

--- index.js
+++ index.js
@@ -1,21 +1,56 @@
-const express = require('express')
-const cors = require('cors')
-const mysql = require('mysql2')
-require('dotenv').config()
+const mysql = require('mysql2');
+const { createHandler } = require('@netlify/functions');
+require('dotenv').config();
 
-const app = express()
-const connection = mysql.createConnection(process.env.DB_URL)
+const connection = mysql.createConnection(process.env.DB_URL);
 
-app.get('/',function(req , res ,next){
-    res.json({msg: 'Hello!!'})
-})
+const handler = async (event, context) => {
+  if (event.httpMethod === 'GET' && event.path === '/') {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ msg: 'Hello!!' }),
+    };
+  }
 
-app.get('/attractions',function(req , res ,next){
-    connection.query('SELECT * FROM attractions',
-        function(err, result, fields){
-            res.json(result)
-        }
-    )
-})
+  if (event.httpMethod === 'GET' && event.path === '/attractions') {
+    return new Promise((resolve, reject) => {
+      connection.query('SELECT * FROM attractions', (err, result) => {
+        if (err) return reject(err);
+        resolve({
+          statusCode: 200,
+          body: JSON.stringify(result),
+        });
+      });
+    });
+  }
 
-app.listen(process.env.PORT || 3000)
+  return {
+    statusCode: 404,
+    body: JSON.stringify({ msg: 'Not Found' }),
+  };
+};
+
+exports.handler = createHandler(handler);
+
+
+// const express = require('express')
+// const cors = require('cors')
+// const mysql = require('mysql2')
+// require('dotenv').config()
+
+// const app = express()
+// const connection = mysql.createConnection(process.env.DB_URL)
+
+// app.get('/',function(req , res ,next){
+//     res.json({msg: 'Hello!!'})
+// })
+
+// app.get('/attractions',function(req , res ,next){
+//     connection.query('SELECT * FROM attractions',
+//         function(err, result, fields){
+//             res.json(result)
+//         }
+//     )
+// })
+
+// app.listen(process.env.PORT || 3000)
